debf98a98fe2beb492072f8ad43242d7e1280834
suraj <surajwakure007@gmail.com>
2024-10-14 18:48:31 +0530

    news application

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./Components/Navbar"
+import NewsBord from "./Components/NewsBord"
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <NewsBord/>
+    </div>
+  )
+}
+
+export default App
